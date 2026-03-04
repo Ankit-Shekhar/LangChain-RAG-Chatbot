bdded554bfd03494a9834bd9f79fd240f5081ad4
Ankit Shekhar <ankitshekhar.23@nshm.edu.in>
2026-03-04 20:10:05 +0530

    setting up git for the project

---
+++ backend/step1.js
@@ -0,0 +1,11 @@
+// define our class
+class PdfQA {
+    // constructor method to initialize the class
+    constructor() {}
+}
+
+// create an instance of the class and store the oblect returned in a variable
+const pdfQA = new PdfQA();
+
+// log our object
+console.log({pdfQA});
